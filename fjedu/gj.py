11cd6e88f2bb7b7822083b3e843c065b59f0479d
iceqiw <qqwei1123@163.com>
2018-03-28 17:59:57 +0800

    aa

--- fjedu/gj.py
+++ fjedu/gj.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import re
+import http.cookiejar
 headers = {
     "Accept":
     "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -9,7 +10,7 @@ headers = {
     "Connection":
     "keep-alive",
     "Cookie":
-    "hbjyUsersCookiesxy.59iedu.com=756|756|21e65008700644948e6166121897395a; UM_distinctid=16261b65d3c3f3-0c3c3a5cd90e4a-3a7f0e5a-1fa400-16261b65d3d19ac; ASP.NET_SessionId=dvn2rkym3rap3iapoy5idrlx; IsLoginUsersCookies_xy.59iedu.comxy.59iedu.com=IsLogin; CNZZDATA5050476=cnzz_eid%3D1971169959-1522054592-null%26ntime%3D1522203047; menu_bind=-1",
+    "hbjyUsersCookiesxy.59iedu.com=756|756|21e65008700644948e6166121897395a; UM_distinctid=16261b65d3c3f3-0c3c3a5cd90e4a-3a7f0e5a-1fa400-16261b65d3d19ac; ASP.NET_SessionId=dvn2rkym3rap3iapoy5idrlx; IsLoginUsersCookies_xy.59iedu.comxy.59iedu.com=IsLogin; CNZZDATA5050476=cnzz_eid%3D1971169959-1522054592-null%26ntime%3D1522213637",
     "Upgrade-Insecure-Requests":
     "1",
     "User-Agent":
@@ -17,16 +18,18 @@ headers = {
 }
 
 timeUrl = '&CurrentTimespan=1060&Id={Id}&SscId=ee58cb9f54644e3cbdd80b8a973f390b&SstId=1497ca5583554eca85fc0aa34e673d43&CourseSdlId=9e4f4f6f655e41b6a85dd72069dd78df&TrainSdlId=0b6d2328e6f145ca973c502a2bf082f3&Type=1&CurrentLength=30'
+session = requests.session()
+session.cookies = http.cookiejar.LWPCookieJar(filename='Cookie')
 
 with open("data.txt", "r") as f:
     for line in f.readlines():
         meId = line.split('\001')[0]
         url_ch = 'http://xy.59iedu.com/Study/Learning/MediaLi?sscId=ee58cb9f54644e3cbdd80b8a973f390b&medId=' + meId
         print(url_ch)
-        r = requests.get(url_ch, headers=headers)
+        r = session.get(url_ch, headers=headers)
         # print(r.text)
         timingUrl_re = 'var timingUrl = "(.*?)"'
-        timingUrl = re.compile(timingUrl_re).findall(r.text)[0]
+        timingUrl = re.compile(timingUrl_re).findall(r.text)[0] 
         id_re = 'Id: "(.*?)"'
         Id = re.compile(id_re).findall(r.text)[0]
         url = timingUrl + timeUrl.format(Id=Id)
@@ -34,7 +37,7 @@ with open("data.txt", "r") as f:
 
         ok = True
         while ok:
-            rt = requests.get(url, headers=headers)
+            rt = session.get(url)  
             print(rt.text)
             id_re = '"Process":(.*?),'
             Id = re.compile(id_re).findall(rt.text)

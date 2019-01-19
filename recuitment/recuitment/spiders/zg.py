db85da36261152428649f4f77590ba366528f65e
wei.qi <wei.qi@thoughtworks.com>
2019-01-19 18:39:58 +0800

    encode

--- recuitment/recuitment/spiders/zg.py
+++ recuitment/recuitment/spiders/zg.py
@@ -17,7 +17,7 @@ class ZgSpider(scrapy.Spider):
         print("start")
         infos = respones.css('#listul li')
         for info in infos:
-            if u'小学' in info.css('a::text').extract_first():
+            if '小学'.encode('utf-8') in info.css('a::text').extract_first():
                 ri = RecuitmentItem()
                 ri['name'] = info.css('a::text').extract_first()
                 if info.xpath('span/text()').extract_first():
